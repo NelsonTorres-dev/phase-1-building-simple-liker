727f56cb4a5b1beb2511e3a8ff06532a6461ddb8
Nelson Torres <ntorres381@gmail.com>
2023-06-05 13:51:08 -0700

    catches error

--- main.js
+++ main.js
@@ -1,8 +1,53 @@
+
+
+//Here's the specification:
+/*
+Add the .hidden class to the error modal in the HTML so it does not appear when the page first loads
+When a user clicks on an empty heart:
+Invoke mimicServerCall to simulate making a server request
+When the "server" returns a failure status:
+Respond to the error using a .catch(() => {}) block after your .then(() => {}) block.
+Display the error modal by removing the .hidden class
+Display the server error message in the modal
+Use setTimeout to hide the modal after 3 seconds (add the .hidden class)
+When the "server" returns a success status:
+Change the heart to a full heart
+Add the .activated-heart class to make the heart appear red
+When a user clicks on a full heart:
+Change the heart back to an empty heart
+Remove the .activated-heart class
+Keep all your styling rules entirely in style.css. Do not manipulate any .style properties.
+Only manipulate the DOM once the server request responds. Do not make the heart full until you're inside a successful .then block.
+Note: The tests will only check for the first part of the specification (adding the hidden class). You should verify the rest of the behavior yourself, by checking the page in the browser.
+*/
+
+
+
 // Defining text characters for the empty and full hearts for you to use later.
 const EMPTY_HEART = '♡'
 const FULL_HEART = '♥'
-
 // Your JavaScript code goes here!
+
+const heartSpans = document.querySelectorAll(".like-glyph")
+heartSpans.forEach((heartSpan) => {
+heartSpan.addEventListener('click', handleHeart)
+})
+
+function handleHeart(e) {
+ const heart = e.target
+  mimicServerCall('fakeURL')
+  .then(console.log('succsses'))
+  .catch(error => {
+   const errorModal = document.querySelector("#modal")
+  //  console.log(errorModal)
+  errorModal.className = ''
+  errorModal.textContent = error
+  setTimeout(() => {
+    errorModal.className = 'hidden'
+  }, 3000);
+  })
+}
+
 
 
 
@@ -23,3 +68,4 @@ function mimicServerCall(url="http://mimicServer.example.com", config={}) {
     }, 300);
   });
 }
+
